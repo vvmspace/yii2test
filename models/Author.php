f85a62b7fbadeebb0a84355f1c7101fb7c339c11
vvmsecured <iva9Yatoj>
2018-02-03 22:59:13 +0500

    Basic models

---
+++ models/Author.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vvm
+ * Date: 03.02.18
+ * Time: 22:55
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+
+class Author extends ActiveRecord
+{
+
+}
---
+++ models/Language.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vvm
+ * Date: 03.02.18
+ * Time: 22:57
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Language extends ActiveRecord
+{
+
+}
